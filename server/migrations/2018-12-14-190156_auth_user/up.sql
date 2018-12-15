0334972349f0ac5aa659d3755e3304e9827093bc
Thomas Forgione <thomas@forgione.fr>
2018-12-15 10:47:31 +0100

    Added unique constraints on username and emails

--- server/migrations/2018-12-14-190156_auth_user/up.sql
+++ server/migrations/2018-12-14-190156_auth_user/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE auth_user (
     id SERIAL PRIMARY KEY,
-    username VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
+    username VARCHAR NOT NULL UNIQUE,
+    email VARCHAR NOT NULL UNIQUE,
     hashed_password VARCHAR NOT NULL,
     activated BOOLEAN NOT NULL,
     activation_key BYTEA
