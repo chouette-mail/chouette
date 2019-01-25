10a28c70c08d8e0503276bc20cf8c8c720a0a72d
Meight <matt.leboucher@gmail.com>
2019-01-25 16:50:59 +0100

    Refactors colors management
    The introduction of the `tesk9/palette` has brought an incompatibility
    with the `Color` type of `elm-ui`. This commit introduces a conversion
    helper.

---
+++ client/src/Colors.elm
@@ -0,0 +1,44 @@
+module Colors exposing (..)
+
+import Color.Generator as Generator
+
+import Color exposing (Color)
+import Element
+
+colorToElement : Color.Color -> Element.Color
+colorToElement =
+    colorToElementWithAlpha 1.0
+
+colorToElementWithAlpha : Float -> Color.Color -> Element.Color
+colorToElementWithAlpha alpha color =
+    let (red, green, blue) = Color.toRGB color 
+    in
+    Element.fromRgb { red = red / 255.0
+                    , green = green / 255.0
+                    , blue = blue / 255.0
+                    , alpha = alpha
+                    }
+
+background : Color 
+background = Color.fromRGB ( 250, 250, 250 )
+
+text : Color
+text = Color.fromRGB ( 137, 143, 163 )
+
+textContrasted : Color
+textContrasted = Color.fromRGB ( 250, 250, 250 )
+
+accentLight : Color
+accentLight = Color.fromRGB (204, 43, 94)
+
+accent : Color
+accent = Color.fromRGB (204, 43, 94)
+
+buttonNormal : Color
+buttonNormal = Color.fromRGB (70, 188, 153)
+
+shadow : Color
+shadow = Color.fromRGB (229, 229, 229)
+
+shadowFocus : Color
+shadowFocus = Generator.tint 35 accentLight
