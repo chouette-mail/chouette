8a9900da458fdf06c4e25f846eda9970d806b89c
Meight <matt.leboucher@gmail.com>
2019-01-26 12:13:03 +0100

    Adds item hover state colors

--- client/src/Colors.elm
+++ client/src/Colors.elm
@@ -5,13 +5,14 @@ import Color.Generator as Generator
 import Color exposing (Color)
 import Element
 
+-- Conversion helpers.
 colorToElement : Color.Color -> Element.Color
 colorToElement =
     colorToElementWithAlpha 1.0
 
 colorToElementWithAlpha : Float -> Color.Color -> Element.Color
 colorToElementWithAlpha alpha color =
-    let (red, green, blue) = Color.toRGB color 
+    let (red, green, blue) = Color.toRGB color
     in
     Element.fromRgb { red = red / 255.0
                     , green = green / 255.0
@@ -19,7 +20,10 @@ colorToElementWithAlpha alpha color =
                     , alpha = alpha
                     }
 
-background : Color 
+-------------------------------------------
+-- Background colors.
+-------------------------------------------
+background : Color
 background = Color.fromRGB ( 250, 250, 250 )
 
 floatingBackground : Color
@@ -27,7 +31,12 @@ floatingBackground = Color.fromRGB ( 255, 255, 255 )
 
 floatingShadow : Color
 floatingShadow = Color.fromRGB ( 245, 245, 245 )
+-------------------------------------------
 
+
+-------------------------------------------
+-- Text colors.
+-------------------------------------------
 text : Color
 text = Color.fromRGB ( 137, 143, 163 )
 
@@ -39,7 +48,12 @@ accentLight = Color.fromRGB (204, 43, 94)
 
 accent : Color
 accent = Color.fromRGB (204, 43, 94)
+-------------------------------------------
 
+
+-------------------------------------------
+-- Button colors.
+-------------------------------------------
 buttonNormal : Color
 buttonNormal = Color.fromRGB (70, 188, 153)
 
@@ -48,3 +62,17 @@ shadow = Color.fromRGB (229, 229, 229)
 
 shadowFocus : Color
 shadowFocus = Generator.tint 35 accentLight
+-------------------------------------------
+
+
+-------------------------------------------
+-- List colors.
+-------------------------------------------
+itemHoverBackground : Color
+itemHoverBackground = Generator.tint 35 accentLight
+
+itemHoverBorder : Color
+itemHoverBorder = accent
+
+itemHoverTextColor : Color
+itemHoverTextColor = text
