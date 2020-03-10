abbf56c7683bc18e89a223a35a81ef462ab7382e
Thomas Forgione <thomas@forgione.fr>
2020-03-10 14:48:46 +0100

    Update elm to 0.19.1

--- client/src/Component/Button.elm
+++ client/src/Component/Button.elm
@@ -1,27 +1,36 @@
 module Component.Button exposing (floatingButton)
 
 import Colors exposing (..)
-import Element exposing (Element, text, paddingXY, focused)
-import Element.Input as Input
+import Element exposing (Element, focused, paddingXY)
 import Element.Background as Background
-import Element.Font as Font
 import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+
 
 floatingButton : String -> Maybe msg -> Element msg
 floatingButton label msg =
-    Input.button [ Element.centerX
-                 , Background.color <| colorToElement Colors.accentLight
-                 , Font.color <| colorToElement Colors.textContrasted
-                 , paddingXY 20 10
-                 , Border.rounded 3
-                 , Border.shadow { offset = (0, 1)
-                                 , size = 3
-                                 , blur = 3
-                                 , color = colorToElement Colors.shadow }
-                 , focused [ Border.shadow { offset = (0, 0)
-                                 , size = 4
-                                 , blur = 0
-                                 , color = colorToElement Colors.shadowFocus } ]]
+    Input.button
+        [ Element.centerX
+        , Background.color <| colorToElement Colors.accentLight
+        , Font.color <| colorToElement Colors.textContrasted
+        , paddingXY 20 10
+        , Border.rounded 3
+        , Border.shadow
+            { offset = ( 0, 1 )
+            , size = 3
+            , blur = 3
+            , color = colorToElement Colors.shadow
+            }
+        , focused
+            [ Border.shadow
+                { offset = ( 0, 0 )
+                , size = 4
+                , blur = 0
+                , color = colorToElement Colors.shadowFocus
+                }
+            ]
+        ]
         { onPress = msg
-        , label = text label
+        , label = Element.text label
         }
